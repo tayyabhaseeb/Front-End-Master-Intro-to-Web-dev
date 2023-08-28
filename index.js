ef5ef198d04321964e2fffc617f20002ee9ddf3e
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-08-28 23:52:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log("Hell0's");
+console.log("Don't do that");
+
+const firstName = "Tayyab";
+const lastName = "Haseeb";
+console.log(`My name is ${firstName} ${lastName}`);
+
+let isTayyabCool = true;
+console.log(isTayyabCool);
+
+let skyIsBlue = false;
+
+// The reason why === is necessary
+if (2 + "2" == 22) {
+  console.log("Sky is blue");
+} else {
+  console.log("Sky is not blue but red");
+}
+
+/////////////////////////////
+
+const friendsAtParty = 10;
+
+if (friendsAtParty === 0) {
+  console.log("You can have some nachoes for yourself");
+} else if (friendsAtParty <= 4) {
+  console.log("A little bit fun for yourself");
+} else {
+  console.log("Part Hard & turn on the music");
+}
+
+////////////////////////////////
+let friendsAtYourParty = 0;
+
+for (let i = 0; i < 10; i++) {
+  console.log(friendsAtYourParty);
+  friendsAtYourParty++;
+}
+
+console.log(friendsAtParty);
